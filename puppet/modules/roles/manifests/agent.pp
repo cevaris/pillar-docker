2dad5926df88b57c47e438eff10316a90ef936f4
Adam Cardenas <cevaris@gmail.com>
2014-11-18 20:31:02 -0700

    got working puppet fig solution to execute code in quick manner

--- puppet/modules/roles/manifests/agent.pp
+++ puppet/modules/roles/manifests/agent.pp
@@ -3,12 +3,18 @@ class roles::agent {
     include base
     # include monit
 
+    $ruby_version = '2.1.2'
+  
     class { 'rbenv':
       install_dir => '/usr/local/rbenv',
-      latest      => '2.1.2'
+      latest      => $ruby_version,
     }
 
+    
     rbenv::plugin { 'sstephenson/ruby-build': }
-    rbenv::build  { '2.1.2': global => true }
+    rbenv::build  { $ruby_version :
+      global => true
+#      onlyif => "ruby -v | grep -c ${ruby_version}",
+    }
 
 }
